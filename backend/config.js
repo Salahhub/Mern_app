aec87f1ca2ba4ab853f4b6bd696c30146770b62c
salaheddine <salahcvc45@gmail.com>
2024-02-20 15:48:59 +0100

    Your commit message

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://ow:salaheddine1966@cluster0.onbwox7.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
