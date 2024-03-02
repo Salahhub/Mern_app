57af9bf686982f9f14087a06a9d60e6a4667d181
Salahhub <112170083+Salahhub@users.noreply.github.com>
2024-03-02 11:19:25 +0100

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('mern-app-7izhq6xb1-salahhub.vercel.app/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
