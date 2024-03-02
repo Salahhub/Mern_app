2759b9f2db86b18ad41b424ceaace5379dba2aec
Salahhub <112170083+Salahhub@users.noreply.github.com>
2024-03-02 11:40:58 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -41,8 +41,6 @@ mongoose
   .connect(mongoDBURL)
   .then(() => {
     console.log('App connected to database');
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
     });
   })
   .catch((error) => {
