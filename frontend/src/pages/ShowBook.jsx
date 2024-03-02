a8adba86ac3688a19e0e87bc136430b952e76c5c
Salahhub <112170083+Salahhub@users.noreply.github.com>
2024-03-02 11:18:53 +0100

    Update ShowBook.jsx

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books/${id}`)
+      .get(`mern-app-7izhq6xb1-salahhub.vercel.app/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
