8071c483d2802f81d362f9a720b3371426452a5b
Salahhub <112170083+Salahhub@users.noreply.github.com>
2024-03-02 11:32:17 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,16 @@ app.use(express.json());
 
 // Middleware for handling CORS POLICY
 // Option 1: Allow All Origins with Default of cors(*)
-app.use(cors());
+app.use(cors(
+      origin:["https://mern-app-frontend-xi.vercel.app/"],
+      methods:["POST","GET","UPDATE","DELETE"],
+      credentials: true,
+
+
+
+
+  
+));
 // Option 2: Allow Custom Origins
 // app.use(
 //   cors({
