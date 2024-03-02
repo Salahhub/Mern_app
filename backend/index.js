61a143bb1c91fc0c94c8438c52d90640976e1dc5
Salahhub <112170083+Salahhub@users.noreply.github.com>
2024-03-02 11:39:34 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // Option 1: Allow All Origins with Default of cors(*)
 app.use(cors({
       origin:["https://mern-app-frontend-xi.vercel.app"],
-      methods:["POST","GET"],
+      methods:["POST","GET","DELETE","UPDATE"],
       credentials: true,
 
 
